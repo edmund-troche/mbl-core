0348198e78f03e8b23ac8938811b8ee4dec8d4e4
Rob Walton <rob.walton@arm.com>
2019-11-25 16:17:37 +0000

    split common functions out from handler

---
+++ firmware-management/swupdate-handlers/common.c
@@ -0,0 +1,126 @@
+#include <mntent.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Read a file to a string.
+// This will not work on files greater than 4Gb as fseek will fail.
+// The in/out param buf is allocated on the heap, it is the caller's
+// responsibility to free the memory allocated to buf.
+int read_file_to_string(const char* const filepath, char** buf)
+{
+    int return_val;
+
+    FILE* fp = fopen(filepath, "r");
+    if (fp == NULL)
+        return 1;
+
+    if (fseek(fp, 0, SEEK_END) == -1)
+    {
+        return_val = 1;
+        goto close;
+    }
+
+    const long file_len = ftell(fp);
+    if (file_len == -1)
+    {
+        return_val = 1;
+        goto close;
+    }
+
+    if (fseek(fp, 0, SEEK_SET) == -1)
+    {
+        return_val = 1;
+        goto close;
+    }
+
+    (*buf) = malloc(file_len + 1);
+    if (!*buf)
+        return 1;
+
+    fread(buf, 1, file_len, fp);
+    if (feof(fp) != 0 || ferror(fp) != 0)
+    {
+        return_val = 1;
+        goto close;
+    }
+
+    *buf[file_len] = '\0';
+    return_val = 0;
+
+close:
+    if (fclose(fp) == -1)
+        return 1;
+
+    return return_val;
+}
+
+int string_endswith(const char* const substr, const char* const fullstr)
+{
+    const size_t endlen = strlen(fullstr) - strlen(substr);
+    if (endlen <= 0)
+        return 1;
+    return strncmp(&fullstr[strlen(fullstr) - strlen(substr)], substr, strlen(substr));
+}
+
+int concat_strs(const char* const str_a, const char* const str_b, char** buf)
+{
+    const size_t buf_size = sizeof(str_a) + sizeof(str_b) + 1;
+    (*buf) = malloc(buf_size);
+    if (!*buf)
+        return 1;
+    snprintf(*buf, buf_size, "%s%s", str_a, str_b);
+    return 0;
+}
+
+int get_mounted_device_filename(const char* const mount_point, char** buf)
+{
+    FILE *mtab = setmntent("/etc/mtab", "r");
+    struct mntent *mntent_desc;
+
+    while ((mntent_desc = getmntent(mtab)))
+    {
+        if (strcmp(mount_point, mntent_desc->mnt_dir) == 0)
+        {
+            const size_t buf_size = strlen(mntent_desc->mnt_fsname) + 1;
+            (*buf) = malloc(buf_size);
+            if (!strncpy(*buf, mntent_desc->mnt_fsname, buf_size-1))
+                return 1;
+
+            *buf[buf_size-1] = '\0';
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int find_target_partition(const char *const mnt_fsname
+                          , const char *const bank1_part_num
+                          , const char *const bank2_part_num
+                          , char** output_buf)
+{
+    char str_to_tokenise[strlen(mnt_fsname) + 1];
+    if (!strncpy(str_to_tokenise, mnt_fsname, strlen(mnt_fsname)))
+        return 1;
+
+    str_to_tokenise[strlen(mnt_fsname)] = '\0';
+    static const char* const delim = "p";
+    char* token;
+    token = strtok(str_to_tokenise, delim);
+
+    if (token == NULL)
+        return 1;
+
+    if (string_endswith(mnt_fsname, bank1_part_num))
+    {
+        return concat_strs(token, bank2_part_num, output_buf);
+    }
+    else if (string_endswith(mnt_fsname, bank2_part_num))
+    {
+        return concat_strs(token, bank1_part_num, output_buf);
+    }
+
+    return 1;
+}
+
---
+++ firmware-management/swupdate-handlers/common.h
@@ -0,0 +1,19 @@
+// Read a file to a string.
+// This will not work on files greater than 4Gb as fseek will fail.
+// The in/out param buf is allocated on the heap, it is the caller's
+// responsibility to free the memory allocated to buf.
+int read_file_to_string(const char* const filepath, char** buf);
+
+int string_endswith(const char* const substr, const char* const fullstr);
+
+int concat_strs(const char* const str_a, const char* const str_b, char** buf);
+
+int get_mounted_device_filename(const char* const mount_point, char** buf);
+
+
+int find_target_partition(const char *const mnt_fsname
+                          , const char *const bank1_part_num
+                          , const char *const bank2_part_num
+                          , char** output_buf);
+
+
